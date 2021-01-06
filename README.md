279496e4e624f01679e674766a2d34691d0e1d28
SolarMotion <ywc92@hotmail.com>
2021-01-06 23:14:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# example-project-google-form-seednet-body-temperature-automation
+
+## Example for post to Google Form using .NET 5 Web Api
---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace google_form_seednet_body_temperature_automation
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
